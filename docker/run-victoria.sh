1377fc48815fb6e1f1690dabbbbccb3a2ad50c2c
Gar|k <garik.djan@gmail.com>
2025-03-19 22:28:19 +0300

    fix: linter

--- docker/run-victoria.sh
+++ docker/run-victoria.sh
@@ -3,10 +3,10 @@
 source ./logging.sh
 
 run_with_logging "VictoriaMetrics ${VICTORIA_METRICS_VERSION}" "${ENABLE_LOGS_VICTORIA_METRICS:-false}" ./victoria-metrics/victoria-metrics-prod \
-  -storageDataPath=/data/victoria-metrics \
-  -retentionPeriod=14d \
-  -maxConcurrentInserts=4 \
-  -httpListenAddr=:8428 \
-  -search.latencyOffset=30s \
-  -memory.allowedPercent=50 \
-  -selfScrapeInterval=10s
+	-storageDataPath=/data/victoria-metrics \
+	-retentionPeriod=14d \
+	-maxConcurrentInserts=4 \
+	-httpListenAddr=:8428 \
+	-search.latencyOffset=30s \
+	-memory.allowedPercent=50 \
+	-selfScrapeInterval=10s
